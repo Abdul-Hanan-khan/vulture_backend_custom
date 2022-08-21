a5e5a84ae538a2c6de54d703424fc07134601474
Faysaş <faysal@handymust.co.uk>
2022-08-20 19:56:04 -0700

    update successfull

--- routes/api.js
+++ routes/api.js
@@ -15,15 +15,29 @@ router.post('/ninjas', function (req, res, next) {
   Ninga.create(req.body).then(function (ninja) {
     res.json({
       "status": true,
+      "msg":"ninga created successfully",
       "data": ninja
     });
   }).catch(next);
 });
 
 
-router.put('/ninjas/:id', function (req, res, next) {
 
-  res.send({ type: "PUT" })
+router.put('/ninjas/:id', function (req, res, next) {
+  Ninga.findByIdAndUpdate({ _id: req.params.id},req.body).then(function(ninjaaaaa){
+
+    Ninga.findOne({ _id: req.params.id}).then(function(ninjaUpdated){
+      res.json({
+        "status":true,
+        "msg":"ninga updated successfully",
+        "data":ninjaUpdated
+      })
+    });
+  });
+
+
+
+  // res.send({ type: "PUT" })
 });
 
 
@@ -44,11 +58,24 @@ router.put('/ninjas/:id', function (req, res, next) {
 
 router.delete('/ninjas/:id', function (req, res, next) {
   Ninga.findByIdAndRemove({ _id: req.params.id }).then(function (ningaaa) {
-    res.json({
-      "status": true,
-      'data':ningaaa
 
-    });
+    if(ningaaa == null){
+      res.json({
+        "status": false,
+        "msg":"failed to delete ninga",
+        'data':ningaaa
+  
+      });
+    }else{
+      res.json({
+        "status": true,
+        "msg":"ninga deleted successfully",
+        'data':ningaaa
+  
+      });
+    }
+
+
   });
   // console.log(req.params.id);
 
