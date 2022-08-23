aa33891003438b15049ee5f7860364bb455f3cf0
Faysaş <faysal@handymust.co.uk>
2022-08-22 19:43:13 -0700

    form data accpted

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 // const jwt = req("jsonwebtoken");
 const jwt = require("jsonwebtoken");
 require("dotenv/config");
+const multiparty = require('multiparty');
 
 
 const User = require("../models/user");
@@ -43,7 +44,7 @@ router.post("/signup", (req, res, next) => {
               address: req.body.address,
             });
 
-            jwt.sign({ user }, process.env.SECRETE_KEY,(err, token) => {
+            jwt.sign({ user }, process.env.SECRETE_KEY, (err, token) => {
               user.token = token;
             })
             user
@@ -71,27 +72,50 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post('/login', (req, res) => {
-console.log(req.body['email']);
-console.log(req.body['password']);
-
-User.findOne({ email: req.body['email'] , password : req.body['password']}).then(function (foundUser) {
-  if (foundUser == null) {
-    res.json({
-      "status": false,
-      "msg": "no user found with specefied email ",
-      "data": []
-    });
-  } else {
-    res.json({
-      "status": true,
-      "msg": "User fetched successfull",
-      "data": foundUser
-    });
-  }
-});
+  User.findOne({ email: req.body['email'], password: req.body['password'] }).then(function (foundUser) {
+    if (foundUser == null) {
+      res.json({
+        "status": false,
+        "msg": "no user found with specefied email ",
+        "data": []
+      });
+    } else {
+      res.json({
+        "status": true,
+        "msg": "User fetched successfull",
+        "data": foundUser
+      });
+    }
+  });
 
 });
 
+
+
+router.post("/test", (req, res, next) => {
+  var form = new multiparty.Form();
+  form.parse(req, function (err, fields, files) {
+    if (req.body['email'] == null || req.body['email'] == "") {
+
+      showResponse(
+        res = res,
+        dataStatus = false,
+        message = "email is required",
+        data = []);
+    } else {
+      const emailValidation= req.body['email'].contains('@');
+      if(emailValidation == false){
+        console.log("email is invalid");
+      }else{
+        console.log("email is ok");
+
+      }
+      showResponse(res,true,"data loaded successfully",req.body)
+    }
+
+  });
+  next();
+});
 
 router.delete("/:userId", (req, res, next) => {
   User.remove({ _id: req.params.userId })
@@ -108,28 +132,13 @@ router.delete("/:userId", (req, res, next) => {
       });
     });
 });
-function verifyToken(req, res, next) {
-  // Get auth header value
-  const bearerHeader = req.headers['authorization'];
-  // console.log(req.token);
-  // Check if bearer is undefined
-  if (typeof bearerHeader !== 'undefined') {
-    // Split at the space
-    const bearer = bearerHeader.split(' ');
-    // Get token from array
-    const bearerToken = bearer[1];
 
-    // Set the token
-    req.token = bearerToken;
-    // console.log(req.token);
-    // Next middleware
-    next();
-  } else {
-    // Forbidden
-    res.sendStatus(403);
-  }
-
+function showResponse(res, dataStatus, message, data) {
+  res.json({
+    "status": dataStatus,
+    "message": message,
+    "data": data
+  });
 }
-
 
 module.exports = router;
